8f74237e01a488d189f9360f2f8a0cc24c00af22
Nutcase810 <jontysvanborg@gmail.com>
2017-06-05 14:39:15 +0930

    added 'tasks' and 'oneNote'. Not sure if i did it properly as i had to do it in NotePad++

---
+++ Tafe Buddy/tasks.swift
@@ -0,0 +1,38 @@
+//
+// oneNote.swift
+//
+// Created by Jonty Svanborg 5/6/2017
+
+import UIKit
+
+class OneNote: UIViewController
+{
+	@IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var timeTableWebView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url  = URL(string:"https://tafesaedu-my.sharepoint.com/_layouts/15/MySite.aspx?MySiteRedirect=AllDocuments")
+        let requestObject = URLRequest(url: url!)
+        self.timeTableWebView.loadRequest(requestObject)
+        timeTableWebView.scrollView.bounces = false
+        self.navigationController?.navigationBar.isTranslucent = false
+        //navigationController?.navigationBar.barTintColor = UIColor.red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //self.navigationController?.hidesBarsOnSwipe = true
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView){
+        loadingIndicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView){
+        loadingIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error){
+        
+    }
+}
